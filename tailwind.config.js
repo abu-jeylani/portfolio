55488d600518823153e670be5dae77a516244272
Abu Mohamed <48966850+moJeylani@users.noreply.github.com>
2022-11-19 14:36:31 -0700

    simplifying and adding dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
@@ -9,6 +9,7 @@ module.exports = {
     extend: {
       fontFamily: {
         burtons: "burtons",
+        poppins: ["Poppins", "sans-serif"],
       },
     },
   },
