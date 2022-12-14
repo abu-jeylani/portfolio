7ad6323c63ff89e88ebb87d48a74f1397333ec8c
Abu Mohamed <48966850+moJeylani@users.noreply.github.com>
2022-12-13 18:22:29 -0700

    adding animation to title

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export default function Home() {
           {darkMode ? (
             <BsSun
               onClick={() => setDarkMode(!darkMode)}
-              className=" cursor-pointer text-xl transition duration-200 hover:scale-125"
+              className=" cursor-pointer text-md transition duration-200 hover:scale-125"
             />
           ) : (
             <BsFillMoonFill
@@ -62,9 +62,10 @@ export default function Home() {
             <div className="pt-10 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto text-center">
               <div className="mb-10">
                 <h1 className="mb-1 font-mono text-4xl text-black-100 md:text-6xl">
-                  <span className="inline-flex h-20 pt-2 overflow-x-hidden whitespace-nowrap text-brand-accent">
-                    hi, I&apos;m Abu 👋
+                  <span className="inline-flex h-20 pt-2  overflow-x-hidden animate-type group-hover:animate-type-reverse whitespace-nowrap text-brand-accent will-change-transform">
+                    hi, I&apos;m Abu
                   </span>
+                  <span className="box-border inline-block w-1 h-10 ml-2 -mb-2 bg-white md:-mb-4 md:h-16 animate-cursor will-change-transform"></span>{" "}
                 </h1>
               </div>
               <p className="text-md pb-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto  text-center">
